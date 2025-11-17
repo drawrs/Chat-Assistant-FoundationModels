76be7148b306865a1244a0f3171a55aa8fe78d04
Rizal Hilman <rizal.hilman67@gmail.com>
2025-11-17 07:37:22 +0700

    Initial Commit

---
+++ Chat Assistant/Chat_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_AssistantApp.swift
+//  Chat Assistant
+//
+//  Created by Rizal Hilman on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Chat_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chat Assistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chat Assistant
+//
+//  Created by Rizal Hilman on 17/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
